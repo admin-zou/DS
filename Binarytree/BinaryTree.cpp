a661b0e4a99abd43a2fcbb2fcfd67e9285af7ff0
centos <admin@bogon.(none)>
2016-04-15 22:07:05 +0800

    二叉树代码

---
+++ Binarytree/BinaryTree.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "BinaryTree.h"
+
+int main()
+{
+	char *preorder = "123456";
+	char *inorder = "324165";
+	char *postorder = "342651";
+
+	BinaryTree<char> BT1("");
+	BT1.IP_BackBinrayTree(postorder, inorder);
+	BT1.PreOrder();
+	BT1.InOder();
+	BT1.PostOrder();
+
+	BinaryTree<char> T("123##4##56");
+	BinaryTree<char> BT("");
+	BT.PI_BackBinaryTree(preorder, inorder);
+	BT.PreOrder();
+	BT.InOder();
+	BT.PostOrder();
+
+	BinaryTree<char> T2(T);
+	BinaryTree<char> T3("123");
+	T3 = T;
+	T.PreOrder();
+	T2.PreOrder();
+	T3.PreOrder();
+
+	T.PreOrder();
+	T.InOder();
+	T.PostOrder();
+	T.LevelOrder();
+
+	T.DPreOrder();
+	T.DInOder();
+	T.DPostOrder();
+	T.size();
+	T.Deep();
+	return 0;
+}
